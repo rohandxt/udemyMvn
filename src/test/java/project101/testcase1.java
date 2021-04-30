75b457c9cd451b6b515ea437bc83138f6026be0e
Rohan Dixit <rohandxt17@gmail.com>
2021-04-30 10:25:53 +0530

    first commit

---
+++ src/test/java/project101/testcase1.java
@@ -0,0 +1,42 @@
+package project101;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class testcase1 {
+
+	@BeforeTest
+	public void getDriver()
+
+	{
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+	@Test
+	public void nowClick() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[1]/div/div[1]/a")).click();
+
+	}
+	
+	@AfterTest
+	public void closing() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.quit();
+		
+	}
+
+}
